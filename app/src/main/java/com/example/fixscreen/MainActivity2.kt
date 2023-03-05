b0248f53ef2def77178ac73170c1d280e75bd9b6
guyueliang <1582086036@qq.com>
2023-03-05 11:51:41 +0800

    Dynamically show and hide stripes according to screen orientation.

--- app/src/main/java/com/example/fixscreen/MainActivity2.kt
+++ app/src/main/java/com/example/fixscreen/MainActivity2.kt
@@ -9,9 +9,7 @@ import android.view.View
 import android.widget.CompoundButton
 import android.widget.SeekBar
 import android.widget.Switch
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.materialswitch.MaterialSwitch
 import com.lzf.easyfloat.EasyFloat
 import com.lzf.easyfloat.enums.ShowPattern
 import com.lzf.easyfloat.interfaces.OnPermissionResult
@@ -19,36 +17,110 @@ import com.lzf.easyfloat.permission.PermissionUtils
 import com.lzf.easyfloat.utils.DisplayUtils
 
 class MainActivity2 : AppCompatActivity() {
+     companion object {
+        const val fixScreen1:String = "fixScreen"
+        const val fixScreen2:String = "fixScreen2"
+    }
+
     @SuppressLint("UseSwitchCompatOrMaterialCode")
-    private lateinit var  switch:Switch
+    private lateinit var switch1: Switch
+    private lateinit var switch2: Switch
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("debug","onCreate()")
+        Log.d("debug", "onCreate()")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if(!PermissionUtils.checkPermission(this)){
+        if (!PermissionUtils.checkPermission(this)) {
             requestPermission();
         }
-        val seekbar = findViewById<SeekBar>(R.id.seekBar)
+        val seekbar1 = findViewById<SeekBar>(R.id.seekBar1)
 //        val divider = findViewById<View>(R.id.divider)
-        switch = findViewById<Switch>(R.id.switch2)
+        switch1 = findViewById<Switch>(R.id.switch1)
+
+        val seekBar2 = findViewById<SeekBar>(R.id.seekBar2)
+        switch2 = findViewById(R.id.switch2)
 
 
-        EasyFloat.with(this).setLayout(R.layout.item_floating)
-            .setTag("fixScreen")
+        //条纹1只在竖屏下显示
+        EasyFloat.with(this)
+            .setLayout(R.layout.item_floating) {
+                it.addOnLayoutChangeListener(object : View.OnLayoutChangeListener{
+                    override fun onLayoutChange(
+                        v: View?,
+                        left: Int,
+                        top: Int,
+                        right: Int,
+                        bottom: Int,
+                        oldLeft: Int,
+                        oldTop: Int,
+                        oldRight: Int,
+                        oldBottom: Int
+                    ) {
+                        //left,right,top,bottom是view自身边界，而不是位置
+//                        Log.d("debug","new pos:left=$left,top=$top,right=$right,bottom=$bottom")
+//                        Log.d("debug","old pos: oldLeft=$oldLeft,oldTop=$oldTop,oldRight=$oldRight,oldBottom=$oldBottom")
+
+                        var x = DisplayUtils.getScreenSize(applicationContext).x //屏幕宽度
+                        var y = DisplayUtils.getScreenSize(applicationContext).y//屏幕高度
+                        val divider = it.findViewById<View>(R.id.divider)
+                        if(x>y){
+                            //横屏
+//                            divider.setBackgroundColor(resources.getColor(R.color.black))
+                            divider.visibility = View.INVISIBLE
+
+                        }else{
+                            //竖屏
+//                            divider.setBackgroundColor(resources.getColor(R.color.black))
+                            divider.visibility = View.VISIBLE
+                        }
+                        Log.d("debug","screenSize:x=$x,y=$y")
+//                        val floatView = EasyFloat.getFloatView("fixScreen")
+//
+//                        var posx = (floatView?.x)?.toInt()
+//                        var posy = (floatView?.y)?.toInt();
+//                        Log.d("debug","view pos:posx=$posx,posy=$posy")
+//
+//                        val divider = it.findViewById<View>(R.id.divider)
+//                        var dposx = divider.x
+//                        var dposy = divider.y
+//                        Log.d("debug","divider pos:posx=$dposx,posy=$dposy")
+                        //下面的方法只能在activity界面内实现
+//                        val isPortrait =
+//                            resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT
+//                        Log.d("debug","isPortrait=$isPortrait")
+//                        val divider = it.findViewById<View>(R.id.divider)
+//                        if(!isPortrait){
+//                            divider.setBackgroundColor(resources.getColor(R.color.purple_200))
+////                            divider.visibility = View.VISIBLE
+////                            it.visibility = View.VISIBLE
+////                            it.setBackgroundColor(resources.getColor(R.color.purple_200))
+//                        }else{
+////                            it.setBackgroundColor(resources.getColor(R.color.black))
+//                            divider.setBackgroundColor(resources.getColor(R.color.black))
+////                            divider.visibility = View.INVISIBLE
+////                            it.visibility = View.INVISIBLE
+//                        }
+                    }
+
+                })
+
+            }
+            .setTag(fixScreen1)
             .setShowPattern(ShowPattern.ALL_TIME)
-            .setLocation(100,200)
-            .setMatchParent(widthMatch = false,heightMatch = true)
+            .setLocation(100, 200)
+            .setMatchParent(widthMatch = false, heightMatch = true)
             .setLayoutChangedGravity(Gravity.CENTER_HORIZONTAL)
             .setImmersionStatusBar(true)
             .show()
 
-        seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+
+        seekbar1.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
 //
 //                Log.d("debug","value=$progress")
-                val floatView = EasyFloat.getFloatView("fixScreen")
+                val floatView = EasyFloat.getFloatView(fixScreen1)
                 val divider = floatView?.findViewById<View>(R.id.divider)
                 if (divider != null) {
                     val layoutparams = divider.layoutParams
@@ -80,18 +152,97 @@ class MainActivity2 : AppCompatActivity() {
 //
             }
         })
-        switch.setOnCheckedChangeListener(object : CompoundButton.OnCheckedChangeListener{
+        switch1.setOnCheckedChangeListener(object : CompoundButton.OnCheckedChangeListener {
             override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
-                Log.d("debug","test")
-                if(switch.isChecked){
-                    Log.d("debug","can't drag")
+                Log.d("debug", "test")
+                if (switch1.isChecked) {
+                    Log.d("debug", "can't drag")
 //                   Toast.makeText(, "checked", Toast.LENGTH_SHORT).show()
-                    EasyFloat.dragEnable(false,"fixScreen")
-                }else{
+                    EasyFloat.dragEnable(false, fixScreen1)
+                } else {
 
 //                   Toast.makeText(this, "unChecked", Toast.LENGTH_SHORT).show()
-                    Log.d("debug","can drag")
-                    EasyFloat.dragEnable(true,"fixScreen")
+                    Log.d("debug", "can drag")
+                    EasyFloat.dragEnable(true, fixScreen1)
+                }
+            }
+
+        })
+
+        //条纹2只在横屏下显示
+        EasyFloat.with(this)
+            .setLayout(R.layout.item_floating) {
+                it.addOnLayoutChangeListener(object : View.OnLayoutChangeListener{
+                    override fun onLayoutChange(
+                        v: View?,
+                        left: Int,
+                        top: Int,
+                        right: Int,
+                        bottom: Int,
+                        oldLeft: Int,
+                        oldTop: Int,
+                        oldRight: Int,
+                        oldBottom: Int
+                    ) {
+                        var x = DisplayUtils.getScreenSize(applicationContext).x //屏幕宽度
+                        var y = DisplayUtils.getScreenSize(applicationContext).y//屏幕高度
+                        val divider = it.findViewById<View>(R.id.divider)
+                        if(x>y){
+                            //横屏
+//                            divider.setBackgroundColor(resources.getColor(R.color.purple_200))
+                            divider.visibility = View.VISIBLE
+
+                        }else{
+                            //竖屏
+//                            divider.setBackgroundColor(resources.getColor(R.color.purple_200))
+                            divider.visibility = View.INVISIBLE
+                        }
+                        Log.d("debug","screenSize:x=$x,y=$y")
+
+                    }
+
+                })
+
+            }
+            .setTag(fixScreen2)
+            .setShowPattern(ShowPattern.ALL_TIME)
+            .setLocation(100, 200)
+            .setMatchParent(widthMatch = false, heightMatch = true)
+            .setLayoutChangedGravity(Gravity.CENTER_HORIZONTAL)
+            .setImmersionStatusBar(true)
+            .show()
+
+        seekBar2.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val floatView = EasyFloat.getFloatView(fixScreen2)
+                val divider = floatView?.findViewById<View>(R.id.divider)
+                if (divider != null) {
+                    val layoutparams = divider.layoutParams
+                    layoutparams.width = progress
+                    divider.layoutParams = layoutparams
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+        switch2.setOnCheckedChangeListener(object : CompoundButton.OnCheckedChangeListener{
+            override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+                if (switch2.isChecked) {
+                    Log.d("debug", "can't drag")
+//                   Toast.makeText(, "checked", Toast.LENGTH_SHORT).show()
+                    EasyFloat.dragEnable(false, fixScreen2)
+                } else {
+
+//                   Toast.makeText(this, "unChecked", Toast.LENGTH_SHORT).show()
+                    Log.d("debug", "can drag")
+                    EasyFloat.dragEnable(true, fixScreen2)
                 }
             }
 
@@ -103,19 +254,19 @@ class MainActivity2 : AppCompatActivity() {
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
 
-        EasyFloat.updateFloat("fixScreen",DisplayUtils.getScreenWidth(this)/2,0)
+//        EasyFloat.updateFloat("fixScreen", DisplayUtils.getScreenWidth(this) / 2, 0)
         // Checks the orientation of the screen
-        if (newConfig.orientation === Configuration.ORIENTATION_LANDSCAPE) {
-            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show()
-        } else if (newConfig.orientation === Configuration.ORIENTATION_PORTRAIT) {
-            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show()
-        }
+//        if (newConfig.orientation === Configuration.ORIENTATION_LANDSCAPE) {
+//            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show()
+//        } else if (newConfig.orientation === Configuration.ORIENTATION_PORTRAIT) {
+//            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show()
+//        }
     }
 
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("debug","onDestroy")
+        Log.d("debug", "onDestroy")
 //        EasyFloat.dismiss("fixScreen")
     }
 
--- app/src/main/java/com/example/fixscreen/myApp.kt
+++ app/src/main/java/com/example/fixscreen/myApp.kt
@@ -1,11 +1,56 @@
 package com.example.fixscreen
 
 import android.app.Application
+import android.content.res.Configuration
+import android.util.Log
+import android.view.Gravity
+import android.view.View
 import com.lzf.easyfloat.EasyFloat
+import com.lzf.easyfloat.enums.ShowPattern
 
 class myApp : Application() {
     override fun onCreate() {
         super.onCreate()
-//        EasyFloat.with(this).setLayout(R.layout.item_floating).show()
+//        EasyFloat.with(this).setLayout(R.layout.item_floating) {
+//
+//            it.addOnLayoutChangeListener(object : View.OnLayoutChangeListener{
+//                override fun onLayoutChange(
+//                    v: View?,
+//                    left: Int,
+//                    top: Int,
+//                    right: Int,
+//                    bottom: Int,
+//                    oldLeft: Int,
+//                    oldTop: Int,
+//                    oldRight: Int,
+//                    oldBottom: Int
+//                ) {
+//
+//                    val isPortrait =
+//                        resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT
+//                    Log.d("debug"," application isPortrait=$isPortrait")
+//                    val divider = it.findViewById<View>(R.id.divider)
+//                    if(!isPortrait){
+//                        divider.setBackgroundColor(resources.getColor(R.color.white))
+//                        divider.visibility = View.VISIBLE
+//                        it.visibility = View.VISIBLE
+////                            it.setBackgroundColor(resources.getColor(R.color.purple_200))
+//                    }else{
+////                            it.setBackgroundColor(resources.getColor(R.color.black))
+//                        divider.setBackgroundColor(resources.getColor(R.color.black))
+//                        divider.visibility = View.INVISIBLE
+//                        it.visibility = View.INVISIBLE
+//                    }
+//                }
+//
+//            })
+//              }
+//            .setTag("fixScreen2")
+//            .setShowPattern(ShowPattern.ALL_TIME)
+//            .setLocation(300, 200)
+//            .setMatchParent(widthMatch = false, heightMatch = true)
+//            .setLayoutChangedGravity(Gravity.CENTER_HORIZONTAL)
+//            .setImmersionStatusBar(true)
+//            .show()
     }
 }
